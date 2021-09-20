84324ba44070d2e9ee070e119f5162a3d129d0b6
shakexxmmm <pocky722+git@gmail.com>
2021-09-21 00:28:39 +0900

    feat: メニューの動きを実装、XRの時にmv画像が切れるのを修正

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function(){
+    $("#js-navButton").on('click', function(){
+        $("#js-navButton").toggleClass("open")
+        // 開閉時に別々のアニメーションを設定するために、openとcloseクラスをつける
+        if($(".header__nav").hasClass("open")) {
+            $(".header__nav").removeClass("open")
+            $(".header__nav").addClass("close")
+        }else{
+            $(".header__nav").removeClass("close")
+            $(".header__nav").addClass("open")
+        }
+    })
+
+    // アンケーリンクのスムーススクロール
+    $('a[href^="#"]').click(function(){
+        // メニューを閉じる
+        $("#js-navButton").removeClass("open")
+        $(".header__nav").removeClass("open")
+        $(".header__nav").addClass("close")
+        // 指定箇所までスクロール
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").delay(400).animate({scrollTop:position}, speed, "swing");
+        return false;
+      });
+})
